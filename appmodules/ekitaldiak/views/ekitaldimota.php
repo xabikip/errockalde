1e4ab2ada56ce3eb44e0bf3d516ce211ba1618c9
xabi <xabikip@gmail.com>
2014-04-17 16:10:48 +0200

    Ekitaldi mota zerrendatzeko collectorviewer plugina erabili

--- appmodules/ekitaldiak/views/ekitaldimota.php
+++ appmodules/ekitaldiak/views/ekitaldimota.php
@@ -7,21 +7,20 @@ class EkitaldiMotaView {
         $form->add_text('deitura', 'Ekitaldi mota');
         $form->add_submit('Ekitaldi mota gehitu');
         $str = $form->show();
-        print Template('EkitaldiMota gehitu')->show($str);
+        print Template('Ekitaldi Mota gehitu')->show($str);
     }
 
     public function editar($obj=array()) {
         $str = file_get_contents(
             STATIC_DIR . "html/ekitaldiak/ekitaldimota_editar.html");
         $html = Template($str)->render($obj);
-        print Template('Editar EkitaldiMota')->show($html);
+        print Template('Editar Ekitaldi Mota')->show($html);
     }
 
     public function listar($coleccion=array()) {
-        $str = file_get_contents(
-            STATIC_DIR . "html/ekitaldiak/ekitaldimota_listar.html");
-        $html = Template($str)->render_regex('LISTADO', $coleccion);
-        print Template('Listado de EkitaldiMota')->show($html);
+        $tabla = CollectorViewer($coleccion, 'ekitaldiak',  'ekitaldimota',
+            False, True, True)->get_table();
+        print Template('Ekitaldi moten zerrenda')->show($tabla);
     }
 }
 
