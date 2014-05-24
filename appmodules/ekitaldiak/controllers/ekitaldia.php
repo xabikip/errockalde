fa3798ad324fc63af661c7309701684d30dd656b
xabi <xabikip@gmail.com>
2014-05-24 19:06:32 +0200

    Refactorizar metodo guardar kitaldi

--- appmodules/ekitaldiak/controllers/ekitaldia.php
+++ appmodules/ekitaldiak/controllers/ekitaldia.php
@@ -22,56 +22,44 @@ class EkitaldiaController extends Controller {
         $this->view->editar($ekitaldimotak, $this->model);
     }
 
+    private function lekuaGorde(){
+        $lekua = new Lekua();
+        $lekua->izena = get_data('izena');
+        $lekua->herria = get_data('herria');
+        $lekua->helbidea = get_data('helbidea');
+        $lekua->save();
+        return $lekua;
+    }
+
     public function guardar() {
 
-        $id = get_data('id');
-        $ekitaldimota = get_data('ekitaldimota');
-        $data = get_data('data');
-        $izena = get_data('izena');
-        $herria = get_data('herria');
-        $helbidea = get_data('helbidea');
-        $ordua = get_data('ordua');
-        $ekitaldi_izena = get_data('ekitaldi_izena');
-
         $errores = array();
+
         $requeridos = array("data", "ordua", "ekitaldi_izena", "ekitaldimota", "izena");
+        $errores= validar_requeridos($errores, $requeridos);
 
-        foreach ($requeridos as $value) {
-            if ($$value == null) $errores[$value]  = "$value beharrezkoa da";
-        }
-
+        $campoImagen = 'kartela';
         $tipo_permitido = array("image/png", "image/jpeg", "image/gif", "image/bmp", "image/jpg");
         $tipo = isset($_FILES['kartela']['type']) ? $_FILES['kartela']['type'] : "image/jpg";
+        $errores= validar_tipoImagen($errores, $tipo_permitido, $tipo, $campoImagen);
 
-        if (!in_array($tipo, $tipo_permitido) AND $_FILES['kartela']['error'] !== 4){
-            $errores['kartela'] = "Formatua ez da egokia.";
-        }
+        if($errores and get_data('id') == 0)  {$this->agregar($errores);exit;}
+        if($errores and get_data('id') !== 0) {$this->editar($id, $errores);exit;}
 
-        if($errores and $id == 0)  {$this->agregar($errores);exit;}
+        $lekua = $this->lekuaGorde();
 
-        if($errores and $id !== 0) {$this->editar($id, $errores);exit;}
-
-        $lekua = new Lekua();
-        $lekua->izena = $izena;
-        $lekua->herria = $herria;
-        $lekua->helbidea = $helbidea;
-        $lekua->save();
-
-        $this->model->ekitaldia_id = $id;
+        $this->model->ekitaldia_id = get_data('id');
         $this->model->lekua = Pattern::composite('Lekua', $lekua);
-        $this->model->data = $data;
-        $this->model->izena = $ekitaldi_izena;
-        $this->model->ordua = $ordua;
-        $ekitaldimota = Pattern::factory('EkitaldiMota', $ekitaldimota );
+        $this->model->data = get_data('data');
+        $this->model->izena = get_data('ekitaldi_izena');
+        $this->model->ordua = get_data('ordua');
+        $ekitaldimota = Pattern::factory('EkitaldiMota', get_data('ekitaldimota') );
         $this->model->ekitaldimota = Pattern::composite('EkitaldiMota', $ekitaldimota);
         $this->model->save();
 
-        $ruta = WRITABLE_DIR . "/ekitaldiak/ekitaldia/kartelak/{$this->model->ekitaldia_id}";
         //ruta del src para ver la imagen /artxibo?dokumentua=/ekitaldiak/ekitaldia/kartelak/ekitaldia_id
-
-        if(isset($_FILES['kartela']['tmp_name'])){
-            move_uploaded_file($_FILES['kartela']['tmp_name'], $ruta);
-        }
+        $ruta = WRITABLE_DIR . "/ekitaldiak/ekitaldia/kartelak/{$this->model->ekitaldia_id}";
+        guardar_imagen($ruta, $campoImagen);
 
         HTTPHelper::go("/ekitaldiak/ekitaldia/listar");
 
--- common/helper.php
+++ common/helper.php
@@ -4,4 +4,24 @@ function get_data($campo){
       return isset($_POST[$campo]) ? $_POST[$campo] : null;
 }
 
+function validar_requeridos($errores=array(), $requeridos=array()){
+     foreach ($requeridos as $value) {
+            if (get_data($value) == null) $errores[$value]  = "$value beharrezkoa da";
+        }
+     return $errores;
+}
+
+function validar_tipoImagen($errores=array(), $tipo_permitido=array(), $tipo, $campoImagen){
+      if (!in_array($tipo, $tipo_permitido) AND $_FILES[$campoImagen]['error'] !== 4){
+                $errores[$campoImagen] = "Formatua ez da egokia.";
+            }
+      return $errores;
+}
+
+function guardar_imagen($ruta, $campoImagen){
+      if(isset($_FILES[$campoImagen]['tmp_name'])){
+                move_uploaded_file($_FILES[$campoImagen]['tmp_name'], $ruta);
+      }
+}
+
 ?>
