a0144756c64c15cd084d5d8872632cfe9f208b9c
xabi <xabikip@gmail.com>
2014-10-28 23:44:20 +0100

    imagen por defecto añadido

--- dokumentuak.php
+++ dokumentuak.php
@@ -3,7 +3,8 @@
 require_once('settings.php');
 require_once('core/helpers/files.php');
 
-$ruta = WRITABLE_DIR . $_GET['dokumentua'];
+$file = isset($_GET['dokumentua']) ? $_GET['dokumentua'] : 'imagen-por-defecto.gif';
+$ruta = WRITABLE_DIR . $file;
 
 FileManager::show($ruta);
 
